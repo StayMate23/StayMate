9e44bb57cef575f07869fe5722e031673cf62932
Lia <128212527+juliakakas@users.noreply.github.com>
2023-11-03 17:45:16 +0100

    Add files via upload
    ez kimaradt

---
+++ PartnerRepository.java
@@ -0,0 +1,11 @@
+package com.example.stay_mate.repository.partner;
+
+import com.example.stay_mate.model.partner.Partner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PartnerRepository extends JpaRepository<Partner, Integer> {
+    Partner findPartnerById(Integer id);
+    void removeById(Integer id);
+}
