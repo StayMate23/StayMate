c3c31f461e0de7e405f3a1e733e198bee962a084
juliakakas <julia.kakas@gmail.com>
2023-11-30 20:23:38 +0100

    image upload hotel

--- src/main/java/com/example/stay_mate/controller/hotel/HotelController.java
+++ src/main/java/com/example/stay_mate/controller/hotel/HotelController.java
@@ -11,6 +11,14 @@ import com.example.stay_mate.service.partner.PartnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 @Controller
 @RequestMapping("/hotels")
@@ -62,9 +70,15 @@ public class HotelController {
     }
 
     @PostMapping("/create/{partner-id}")
-    public String addHotel(@ModelAttribute("new_hotel") Hotel hotel, @PathVariable("partner-id") Integer partnerId) {
+    public String addHotel(@ModelAttribute("new_hotel") Hotel hotel, @PathVariable("partner-id") Integer partnerId, @RequestParam("image") MultipartFile hotelimage) throws IOException {
         hotel.setPartner(partnerService.getPartnerById(partnerId));
         hotelService.saveHotel(hotel);
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath =
+                Paths.get(System.getProperty("user.dir") + "/src/main/resources/static/uploads",
+                        partnerId + "-" + "hotel" + "-"+ hotel.getName());
+        fileNames.append(partnerId + "-" + "hotel" + "-"+ hotel.getName());
+        Files.write(fileNameAndPath, hotelimage.getBytes());
         return "redirect:/partner/current";
     }
 
--- src/main/java/com/example/stay_mate/repository/hotel/HotelImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.stay_mate.repository.hotel;
-
-import com.example.stay_mate.model.hotel.HotelImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelImageRepository extends JpaRepository<HotelImage,Integer> {
-}
